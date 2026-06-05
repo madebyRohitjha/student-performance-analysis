3777e2d619b50deb63d98d14ad964bc7830d8cd5
madebyrohitjha <rohit.jha.ic@gmail.com>
2026-06-06 02:54:33 +0900

    First ML project

---
+++ README.md
@@ -0,0 +1,34 @@
+# Student Performance Prediction
+
+# Project Overview
+
+This project uses Machine Learning to predict student math scores based on reading and writing scores.
+
+# Technologies Used
+
+* Python
+* Pandas
+* Matplotlib
+* Scikit-Learn
+
+# Machine Learning Workflow
+
+1. Load dataset
+2. Perform Exploratory Data Analysis (EDA)
+3. Select features and target
+4. Split data into training and testing sets
+5. Train a Linear Regression model
+6. Evaluate model performance using Mean Absolute Error (MAE)
+7. Predict math scores for new students
+
+# Results
+
+Mean Absolute Error (MAE): 7.35
+
+Example Prediction:
+
+Reading Score: 80
+
+Writing Score: 75
+
+Predicted Math Score: 74.29
---
+++ studentperformance.py
@@ -0,0 +1,46 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_error
+df = pd.read_csv("studentsperformance.csv")
+#profile = ProfileReport(df)
+#profile.to_file("eda_report.html")
+#print(df.head())
+#print(df.info())
+#print(df[["math score","reading score","writing score"]].describe())
+#print(df.isnull().sum())
+#print(df["reading score"].max())
+#print(df["math score"].min())
+#print(df["math score"].mean())
+#print(df.groupby("gender")[["math score","reading score","writing score"]].mean())
+
+#print(df.groupby("test preparation course")
+#      [["reading score","writing score","math score"]].mean())
+#print(df[["math score","reading score","writing score"]].corr(numeric_only=True))
+#import matplotlib.pyplot as plt
+#avg_score = df[["math score","reading score","writing score"]].mean()
+#plt.bar(avg_score.index,avg_score.values)
+#plt.show()
+#plt.hist(df["math score"])
+#plt.show()
+
+x = df[["reading score","writing score"]]
+
+y = df ["math score"]
+
+X_train, X_test, y_train, y_test = train_test_split( x,y,test_size =0.2,random_state= 42)
+
+model = LinearRegression()
+
+model.fit(X_train,y_train)
+
+predictions = model.predict(X_test)
+
+mae = mean_absolute_error(y_test,predictions)
+
+print("Mean Absolute Error:", mae)
+
+new_student = [[80,75]]
+
+predictions= model.predict(new_student)
+print("predicted math score:", predictions)
