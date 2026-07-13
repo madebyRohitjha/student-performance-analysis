c1f830d2dffa7f7c4b6362404f73de4d589d2522
madebyRohitjha <rohit.jha.ic@gmail.com>
2026-07-13 16:04:34 +0900

    Use all dataset features with one-hot encoding

--- studentperformance.py
+++ studentperformance.py
@@ -106,3 +106,28 @@ coefficients = pd.DataFrame({
 })
 
 print(coefficients)
+
+X = df[["reading score", "writing score"]]
+
+print(df.columns)
+
+X = df.drop("math score", axis=1)
+y = df["math score"]
+X = pd.get_dummies(X, drop_first=True)
+
+print(X.head())
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X,
+    y,
+    test_size=0.2,
+    random_state=42
+)
+
+model = LinearRegression()
+
+model.fit(X_train, y_train)
+
+predictions = model.predict(X_test)
+
+print("MAE:", mean_absolute_error(y_test, predictions))
